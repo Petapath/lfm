663a080eef108bb196c78096f308107db19cab1a
Michal Harasimiuk <michal@petapath.com>
2024-10-17 00:27:36 +0100

    Pinia and local store

--- src/components/c1.vue
+++ src/components/c1.vue
@@ -9,6 +9,8 @@
 	<h5>C1</h5>
 	<div>My width is {{width}}</div>
 	<div>My count is {{counter.count}}</div>
+	<div>My double count is {{counter.doubleCount}}</div>
 	<button @click="width++">Increase width</button>
 	<button @click="counter.increment()">Increase count</button>
+	<button @click="counter.set(100)">Set to a 100</button>
 </template>
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,36 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { useLocalStorage } from '@vueuse/core'
 
+// this is the Option Stores way
+/*
+export const useCounterStore = defineStore('counter', {
+	state: () => ({ count: 0 }),
+	getters: {
+			doubleCount: (state) => state.count * 2,
+	},
+	actions: {
+  	increment() {
+    	this.count++
+  	},
+  	set(nc) {
+    	this.count=nc;
+  	},
+	},
+})
+*/
+
+// this is the Setup Stores way
+//
 export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
+  const count = ref(useLocalStorage('mh',0))
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
   }
+  function set(v) {
+	count.value=v;
+  }
 
-  return { count, doubleCount, increment }
+  return { count, doubleCount, increment, set }
 })
